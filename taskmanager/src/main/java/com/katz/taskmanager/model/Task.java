431357e321736c61e8397bb1a0997b97297c7267
katzan <kashkin.alex@gmail.com>
2013-11-04 14:47:15 +0400

    init commit

---
+++ taskmanager/src/main/java/com/katz/taskmanager/model/Task.java
@@ -0,0 +1,19 @@
+package com.katz.taskmanager.model;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Task {
+
+    @NotNull
+    @Size(max = 40)
+    private String description;
+
+}
---
+++ taskmanager/src/main/java/com/katz/taskmanager/web/TaskController.java
@@ -0,0 +1,12 @@
+package com.katz.taskmanager.web;
+
+import com.katz.taskmanager.model.Task;
+import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/tasks")
+@Controller
+@RooWebScaffold(path = "tasks", formBackingObject = Task.class)
+public class TaskController {
+}
